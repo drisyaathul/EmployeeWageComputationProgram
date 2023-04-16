7f8f2049de3479fc3cd47ca34b8850d1b40bb854
drisyaathul <drisyak809@gmail.com>
2023-04-17 00:00:05 +0800

    Added: Employee Wage For Part Time

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -5,23 +5,31 @@ import java.util.Random;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("*** Welcome To EMPLOYEE WAGE COMPUTATION PROGRAM ***");
-//UC1
+
         Random random = new Random();
-        int attendance = random.nextInt(2);
-        if (attendance == 1)
+        int attendance = random.nextInt(3);
+        if (attendance == 0)
+            System.out.println("Employee is ABSENT");
+        else if (attendance == 1)
             System.out.println("Employee is PRESENT");
         else
-            System.out.println("Employee is ABSENT");
-//UC2
+            System.out.println("Employee is PART-TIME");      //UC3
+
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int partTimeHour = 4;   //UC3
         int dailyWage;
+        int partTimeWage;
 
         dailyWage = attendance * wagePerHour * fullDayHour;
-        if (attendance == 1)
+        partTimeWage = wagePerHour * partTimeHour;     //UC3
+
+        if (attendance == 0)
+            System.out.println("Employee Wage Per Day = "+dailyWage);
+        else if (attendance == 1)
             System.out.println("Employee Wage Per Day = "+dailyWage);
         else
-            System.out.println("Employee Wage Per Day = "+dailyWage);
+            System.out.println("Employee Part Time Wage  = "+partTimeWage);     //UC3
 
     }
 }
