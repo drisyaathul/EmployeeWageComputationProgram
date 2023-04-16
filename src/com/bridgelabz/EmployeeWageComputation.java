4872baef888933afad4585b8e8108b1856eb5589
drisyaathul <drisyak809@gmail.com>
2023-04-17 01:00:00 +0800

    Added: Employee Daily Wage Using Switch Case Statement

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,33 +3,29 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWageComputation {
+
     public static void main(String[] args) {
         System.out.println("*** Welcome To EMPLOYEE WAGE COMPUTATION PROGRAM ***");
-
+        int wagePerHour = 20;
+        int dailyWage;
+        int workingHour;
         Random random = new Random();
         int attendance = random.nextInt(3);
-        if (attendance == 0)
-            System.out.println("Employee is ABSENT");
-        else if (attendance == 1)
-            System.out.println("Employee is PRESENT");
-        else
-            System.out.println("Employee is PART-TIME");      //UC3
-
-        int wagePerHour = 20;
-        int fullDayHour = 8;
-        int partTimeHour = 4;   //UC3
-        int dailyWage;
-        int partTimeWage;
-
-        dailyWage = attendance * wagePerHour * fullDayHour;
-        partTimeWage = wagePerHour * partTimeHour;     //UC3
-
-        if (attendance == 0)
-            System.out.println("Employee Wage Per Day = "+dailyWage);
-        else if (attendance == 1)
-            System.out.println("Employee Wage Per Day = "+dailyWage);
-        else
-            System.out.println("Employee Part Time Wage  = "+partTimeWage);     //UC3
-
+        switch (attendance) {
+            case 0:
+                System.out.println("Employee is ABSENT");
+                workingHour = 0;
+                break;
+            case 1:
+                System.out.println("Employee is PRESENT");
+                workingHour = 8;
+                break;
+            default:
+                System.out.println("Employee is PART-TIME");
+                workingHour = 4;
+                break;
+        }
+        dailyWage = workingHour * wagePerHour;
+        System.out.println("Employee Daily Wage = " +dailyWage);
     }
 }
