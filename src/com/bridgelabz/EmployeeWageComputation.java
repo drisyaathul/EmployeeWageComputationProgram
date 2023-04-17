b8a12fccb8beac901df8505daba6beaa4b9e14c7
drisyaathul <drisyak809@gmail.com>
2023-04-17 12:18:29 +0800

    Added: Employee Wage till a condition of total days reached in a month

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,32 +4,46 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 
+    private static final int wagePerHour = 20;
+    private static final int workingDays = 20;   //given total working days in a month(UC5)
+    private static final int max_workingHours = 100;   //given total working Hours in a month(UC6)
+
     public static void main(String[] args) {
         System.out.println("*** Welcome To EMPLOYEE WAGE COMPUTATION PROGRAM ***");
-        int wagePerHour = 20;
-        int workingDays = 20;   //given total working days in a month(UC5)
-        int monthlyWage;
+
+        int totalWage = 0;
         int dailyWage;
         int workingHour;
+        int totalWorkingHour = 0;
+
+        for (int day = 1; day <= workingDays; day++) {
+
         Random random = new Random();
         int attendance = random.nextInt(3);
-        switch (attendance) {
-            case 0:
-                System.out.println("Employee is ABSENT");
-                workingHour = 0;
-                break;
-            case 1:
-                System.out.println("Employee is PRESENT");
-                workingHour = 8;
-                break;
-            default:
-                System.out.println("Employee is PART-TIME");
-                workingHour = 4;
-                break;
+            System.out.println();
+            System.out.println("DAY => "+day+ ";");
+            switch (attendance) {
+                case 0:
+                    System.out.println("Employee is ABSENT");
+                    workingHour = 0;
+                    break;
+                case 1:
+                    System.out.println("Employee is PRESENT");
+                    workingHour = 8;
+                    break;
+                default:
+                    System.out.println("Employee is PART-TIME");
+                    workingHour = 4;
+                    break;
+            }
+            dailyWage = workingHour * wagePerHour;
+            totalWage += dailyWage ;
+            totalWorkingHour += workingHour;
+            System.out.println("Employee Working Hour Per Day = "+workingHour);
+            System.out.println("Employee Daily Wage = " + dailyWage);
         }
-        dailyWage = workingHour * wagePerHour;
-        monthlyWage = dailyWage * workingDays;
-        System.out.println("Employee Daily Wage = " +dailyWage);
-        System.out.println("Employee Monthly Wage = " +monthlyWage);
+        System.out.println("**********************************************");
+        System.out.println("Employee Monthly Wage = " + totalWage+ " $ ");
+
     }
 }
