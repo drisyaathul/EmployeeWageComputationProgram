4d6eff89a21b848b6cef1976c8cd369da6e1d352
drisyaathul <drisyak809@gmail.com>
2023-04-21 00:57:04 +0800

    Added: Save the Total Wage of Each Company by Using Instance Variable

---
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -0,0 +1,76 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class EmpWageBuilder {
+
+    private final String companyName;
+    private final int wagePerHour ;
+    private final int workingDays ;
+    private final int max_workingHours ;
+/*
+Generating Constructors
+ */
+    public EmpWageBuilder(String companyName, int wagePerHour, int workingDays, int max_workingHours) {
+        this.companyName =companyName;
+        this.wagePerHour = wagePerHour;
+        this.workingDays = workingDays;
+        this.max_workingHours = max_workingHours;
+    }
+    public void ComputeEmpWage(){
+        //   variables
+        int totalWage = 0;
+        int dailyWage;
+        int workingHour;
+        int totalWorkingHour = 0;
+        //computation
+        for (int day = 1; day <= workingDays; day++) {
+ /*
+    * Attendance to check the employee is full tym work or part tym or Absent
+    * We use the switch case For Attendance
+  */
+            Random random = new Random();
+            int attendance = random.nextInt(3);
+            System.out.println();
+            System.out.println("DAY => "+day+ ";");
+            switch (attendance) {
+                case 0:
+                    System.out.println("Employee is ABSENT");
+                    workingHour = 0;
+                    break;
+                case 1:
+                    System.out.println("Employee is PRESENT");
+                    workingHour = 8;
+                    break;
+                default:
+                    System.out.println("Employee is HALF-DAY");
+                    workingHour = 4;
+                    break;
+            }
+ /*
+ calculating the Employee wage
+ * Calculating daily Wage = Working hrs multiply by Wage Per Hrs
+ * Calculating Total Wage = Daily Wage
+ * Calculating  Total working hrs  = Max Working hrs
+  */
+            dailyWage = workingHour * wagePerHour;
+            totalWage += dailyWage ;
+            totalWorkingHour += workingHour;
+            System.out.println("Employee Working Hour Per Day = "+workingHour);
+            System.out.println("Employee Daily Wage = " + dailyWage);
+        }
+        System.out.println();
+        System.out.println("Employee of Monthly Wage is " + totalWage+ " $ ");
+        System.out.println("--------------------------------------");
+    }
+    @Override
+    public String toString() {
+        return "EmpWageBuilder{" +
+                "companyName='" + companyName + '\'' +
+                ", wagePerHour=" + wagePerHour +
+                ", workingDays=" + workingDays +
+                ", max_workingHours=" + max_workingHours +
+                '}';
+    }
+}
+
--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,58 +4,21 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 
-    private static final int wagePerHour = 20;
-    private static final int workingDays = 20;
-    private static final int max_workingHours = 100;
-    public void ComputeEmployeeWage(String companyName, int wagePerHour, int workingDays, int max_workingHours){
-
-        System.out.println("Name of the Company : " +companyName);
-        System.out.println("Company Wage per Hour : " +wagePerHour+ " $");
-        System.out.println("Company Working Days : " +workingDays+ " Days");
-        System.out.println("Company Maximum Working Hours : " +max_workingHours+ " Hrs");
-
-        int totalWage = 0;
-        int dailyWage;
-        int workingHour;
-        int totalWorkingHour = 0;
-
-        for (int day = 1; day <= workingDays; day++) {
-
-            Random random = new Random();
-            int attendance = random.nextInt(3);
-            System.out.println();
-            System.out.println("DAY => "+day+ ";");
-            switch (attendance) {
-                case 0:
-                    System.out.println("Employee is ABSENT");
-                    workingHour = 0;
-                    break;
-                case 1:
-                    System.out.println("Employee is PRESENT");
-                    workingHour = 8;
-                    break;
-                default:
-                    System.out.println("Employee is HALF-DAY");
-                    workingHour = 4;
-                    break;
-            }
-            dailyWage = workingHour * wagePerHour;
-            totalWage += dailyWage ;
-            totalWorkingHour += workingHour;
-            System.out.println("Employee Working Hour Per Day = "+workingHour);
-            System.out.println("Employee Daily Wage = " + dailyWage);
-        }
-        System.out.println();
-        System.out.println("Employee of " +companyName+ " Monthly Wage is " + totalWage+ " $ ");
-        System.out.println("--------------------------------------");
-    }
     public static void main(String[] args) {
 
         System.out.println("*** Welcome To EMPLOYEE WAGE COMPUTATION PROGRAM ***");
-
-        EmployeeWageComputation company = new EmployeeWageComputation();
-        company.ComputeEmployeeWage("ABB", 50, 25, 100);
-        company.ComputeEmployeeWage("TCS", 30, 20, 160);
-        company.ComputeEmployeeWage("INFO PARK", 20, 24, 120);
+        System.out.println();
+ /*
+   Create class method to compute Employee wage And calling the object of Employee wage Builder in the main method
+ */
+        EmpWageBuilder abb = new EmpWageBuilder("ABB",50,25,100);
+        System.out.println(abb);
+        abb.ComputeEmpWage();
+        EmpWageBuilder tcs = new EmpWageBuilder("TCS",30,30,160);
+        System.out.println(tcs);
+        tcs.ComputeEmpWage();
+        EmpWageBuilder infoPark = new EmpWageBuilder("INFO PARK",20,24,120);
+        System.out.println(infoPark);
+        infoPark.ComputeEmpWage();
     }
 }
