d1d917ec07a5d94691aa0193c5950482688a864d
drisyaathul <drisyak809@gmail.com>
2023-04-21 11:44:58 +0800

    Refactored: To manage Employee Wage Of Multiple Companies

--- src/com/bridgelabz/CompanyEmpWage.java
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -1,11 +1,12 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Random;
 
-public class EmpWageBuilder {
+public class CompanyEmpWage {
 /*
-  Use Instance Variable instead of function parameters(UC9)
- */
+ Use Instance Variable instead of function parameters(UC9)
+*/
     private final String companyName;
     private final int wagePerHour ;
     private final int workingDays ;
@@ -13,44 +14,43 @@ public class EmpWageBuilder {
 /*
 Generating Constructors
  */
-    public EmpWageBuilder(String companyName, int wagePerHour, int workingDays, int max_workingHours) {
+    public CompanyEmpWage(String companyName, int wagePerHour, int workingDays, int max_workingHours) {
         this.companyName =companyName;
         this.wagePerHour = wagePerHour;
         this.workingDays = workingDays;
         this.max_workingHours = max_workingHours;
     }
     public void ComputeEmpWage(){
-        //   variables
+    //variables
         int totalWage = 0;
         int dailyWage;
         int workingHour;
         int totalWorkingHour = 0;
-        //computation
+    //computation
         for (int day = 1; day <= workingDays; day++) {
- /*
-    * Attendance to check the employee is full tym work or part tym or Absent
-    * We use the switch case For Attendance
-  */
+/*
+ * Attendance to check the employee is full tym work or part tym or Absent
+ * We use the switch case For Attendance
+ */
             Random random = new Random();
             int attendance = random.nextInt(3);
-            System.out.println();
-            System.out.println("DAY => "+day+ ";");
+            //System.out.println("DAY => "+day+ ";");
             switch (attendance) {
                 case 0:
-                    System.out.println("Employee is ABSENT");
+                    //System.out.println("Employee is ABSENT");
                     workingHour = 0;
                     break;
                 case 1:
-                    System.out.println("Employee is PRESENT");
+                    //System.out.println("Employee is PRESENT");
                     workingHour = 8;
                     break;
                 default:
-                    System.out.println("Employee is HALF-DAY");
+                    //System.out.println("Employee is HALF-DAY");
                     workingHour = 4;
                     break;
             }
  /*
- calculating the Employee wage
+   calculating the Employee wage
  * Calculating daily Wage = Working hrs multiply by Wage Per Hrs
  * Calculating Total Wage = Daily Wage
  * Calculating  Total working hrs  = Max Working hrs
@@ -58,12 +58,11 @@ Generating Constructors
             dailyWage = workingHour * wagePerHour;
             totalWage += dailyWage ;
             totalWorkingHour += workingHour;
-            System.out.println("Employee Working Hour Per Day = "+workingHour);
-            System.out.println("Employee Daily Wage = " + dailyWage);
+//            System.out.println("Employee Working Hour Per Day = "+workingHour);
+//            System.out.println("Employee Daily Wage = " + dailyWage);
         }
-        System.out.println();
-        System.out.println("Employee of Monthly Wage is " + totalWage+ " $ ");
-        System.out.println("--------------------------------------");
+        System.out.println("Employee of "+companyName+" Monthly Wage is " + totalWage+ " $ ");
+        System.out.println("--------------------------------------------------------------");
     }
     @Override
     public String toString() {
--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,7 +1,5 @@
 package com.bridgelabz;
 
-import java.util.Random;
-
 public class EmployeeWageComputation {
 
     public static void main(String[] args) {
@@ -9,15 +7,15 @@ public class EmployeeWageComputation {
         System.out.println("*** Welcome To EMPLOYEE WAGE COMPUTATION PROGRAM ***");
         System.out.println();
  /*
-   Create class method to compute Employee wage And calling the object of Employee wage Builder in the main method
+   Create class method to compute Employee wage And calling the object of Company Employee wage in the main method
  */
-        EmpWageBuilder abb = new EmpWageBuilder("ABB",50,25,100);
+        CompanyEmpWage abb = new CompanyEmpWage("ABB",50,25,100);
         System.out.println(abb);
         abb.ComputeEmpWage();
-        EmpWageBuilder tcs = new EmpWageBuilder("TCS",30,30,160);
+        CompanyEmpWage tcs = new CompanyEmpWage("TCS",30,30,160);
         System.out.println(tcs);
         tcs.ComputeEmpWage();
-        EmpWageBuilder infoPark = new EmpWageBuilder("INFO PARK",20,24,120);
+        CompanyEmpWage infoPark = new CompanyEmpWage("INFO PARK",20,24,120);
         System.out.println(infoPark);
         infoPark.ComputeEmpWage();
     }
